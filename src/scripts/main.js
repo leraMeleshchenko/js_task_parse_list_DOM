a9c77f82c677fa234396b61c0513bb510ea875db
Lera <valeriamelesenko4@gmail.com>
2025-03-08 12:28:55 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+
+const people = Array.from(document.querySelectorAll('li'));
+
+const sortedPeople = people.sort((a, b) => {
+  const salaryA = parseInt(a.getAttribute('data-salary').replace(/[$,]/g, ''));
+  const salaryB = parseInt(b.getAttribute('data-salary').replace(/[$,]/g, ''));
+
+  return salaryB - salaryA;
+});
+
+const ul = document.querySelector('ul');
+
+ul.innerHTML = '';
+
+sortedPeople.forEach((li) => {
+  ul.appendChild(li);
+});
